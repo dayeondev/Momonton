81c109844afc72c1d88fc288b5525d27c1ecc590
dayeon <dayeon.dev@gmail.com>
2019-04-26 12:28:02 +0900

    Try the array system

--- live-wallpaper/newbg.js
+++ live-wallpaper/newbg.js
@@ -1,39 +1,63 @@
 const liveForm = document.querySelector(".js-livewallpaper");
 
-var dayimg = document.querySelector(".bgday")
-var nightimg = document.querySelector(".bgnight");
+// 5400초마다 전환
 
-var image = new Image();
-var image2 = new Image();
+var images = new Array();
 
+var nightMode;
+var alphaValue;
 
-var nightMode = -1;
-var alphaValue = 1;
-
-function setAlpha(){
+function importImages(){
+    console.log(`importImages() called`);
+    for(var i = 0; i < 16; i++){
+        images[i] = new Image();
+        images[i].src = `mojave_dynamic/mojave_dynamic_${i + 1}.jpeg`;
+        images[i].classList.add("liveimg");
+        images[i].classList.add("display_none")
+        liveForm.prepend(images[i]);
+    }
+    
+}
+function setAlpha(imgNum){
+    console.log(`setAlpha(${imgNum}) called`);
     alphaValue = alphaValue + 0.05 * nightMode;
     if(alphaValue < 0 || alphaValue > 1) nightMode = nightMode * -1;
-
-    image.style.opacity = alphaValue;
+    
+    images[imgNum + 1 == 16? 0 : imgNum + 1].style.opacity = alphaValue;
     // image2.style.opacity = 1;
     console.log(alphaValue, nightMode);
 }
 
-function paintImage(){
-
-    image.src = `../images/Day.jpg`;
-    image2.src = `../images/Night.jpg`;
-
-    image.classList.add("bgday");
-    image2.classList.add("bgnight");
-
-    liveForm.prepend(image);
-    liveForm.prepend(image2);
+function changeImage(imgNum){
+    console.log(`changeImage(${imgNum}) called`);
+    images[imgNum].classList.remove("display_none");
+    images[imgNum + 1 == 16? 0 : imgNum + 1].classList.remove("display_none");
+    nightMode = -1;
+    alphaValue = 1;
+    setInterval(setAlpha(imgNum), 50);
 }
 
-function init(){
-    paintImage();
-    setInterval(setAlpha, 50);
+// var nightMode = -1;
+// var alphaValue = 1;
+
+// function setAlpha(){
+//     alphaValue = alphaValue + 0.05 * nightMode;
+//     if(alphaValue < 0 || alphaValue > 1) nightMode = nightMode * -1;
+
+//     // image.style.opacity = alphaValue;
+//     // image2.style.opacity = 1;
+//     console.log(alphaValue, nightMode);
+// }
+
+function paintImage(){
+    importImages();``
+
+    changeImage(10);
+}
+
+function init(){``
+    paintImage();``
+    // setInterval(setAlpha, 50);
 }
 
 init();
